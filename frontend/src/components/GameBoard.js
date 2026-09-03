eec89cc7bebe660fa4e7fbe51d246306f0038b2c
unknown <nuno.ricardo.silva@gmail.com>
2026-09-03 13:22:38 +0100

    Corrigindo importação não utilizada no GameBoard

--- frontend/src/components/GameBoard.js
+++ frontend/src/components/GameBoard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export default function GameBoard({ playerState, onOpenHelp, phaseIntro, isEvaluation, onReady, onEvaluation, socket }) {
   const isTraitor = playerState.role === 'traitor';
